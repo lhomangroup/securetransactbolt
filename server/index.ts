147570417f1a80329d74f7233082e94e9c3919f3
lhomangroup <134020248+lhomangroup@users.noreply.github.com>
2025-07-11 01:23:05 +0200

    Updated package-lock.json

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import pool from '../services/database';
